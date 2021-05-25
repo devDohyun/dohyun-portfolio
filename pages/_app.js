9040893b8c880d74eca2c8d430c0bace72c206ae
devDohyun <dohyun10101@gmail.com>
2021-05-25 20:59:59 +0900

    Init

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import '@/styles/globals.scss'
+
+
+const App = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />
+}
+
+
+export default App
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+const Index = () => {
+  return (
+    <>
+      Init
+    </>
+  )
+}
+
+
+export default Index
