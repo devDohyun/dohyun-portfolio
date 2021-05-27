88ab60ed3ec2b16a8e1eacfd93dac43d51516c0e
devDohyun <dohyun10101@gmail.com>
2021-05-27 22:41:15 +0900

    updated

--- components/Containers/IntroContainer/index.js
+++ components/Containers/IntroContainer/index.js
@@ -1,9 +1,40 @@
+import { useState, useEffect, useRef } from 'react'
 import Intro from '@/components/Intro'
 
 
 const IntroContainer = () => {
+  const [scroll, setScroll] = useState({
+    percentage: 0
+  })
+  const backgroundPlaceholder = useRef()
+
+
+  useEffect(() => {
+    const scrollHandler = () => setScroll((scroll) => ({
+      ...scroll,
+      percentage: Math.min((window.scrollY - backgroundPlaceholder.current.offsetTop) / (backgroundPlaceholder.current.offsetHeight - window.innerHeight), 1)
+    }))
+    
+    
+    window.addEventListener('scroll', scrollHandler)
+
+
+    return () => {
+      window.removeEventListener('scroll', scrollHandler)
+    }
+  }, [])
+  useEffect(() => {
+    console.log(scroll)
+  }, [scroll])
+
+
   return (
-    <Intro></Intro>
+    <Intro
+      refBackgroundPlaceholder={backgroundPlaceholder}
+      backgroundOpacity={(1 - scroll.percentage)}
+      backgroundHeight="200vh"
+      isFixed={true}
+    ></Intro>
   )
 }
 
--- components/Intro/index.js
+++ components/Intro/index.js
@@ -1,15 +1,22 @@
 import style from './index.module.scss'
 
 
-const Intro = (props) => {
-  
-  
+const Intro = ({ backgroundHeight = '100vh', refBackgroundPlaceholder, backgroundOpacity, isFixed  = false }) => {
   return (
     <div className={style.intro}>
-      <div>
-        <div className={style.title}>Front-End 개발자 안도현</div>
-        <div className={style.subtitle}>아래로 내려서 더-보기</div>
+      <div className={style.content} style={{ position: isFixed ? 'fixed' : '' }}>
+        <div className={style.content_text}>
+          <div className={style.title}>3년차 Front-End 개발자 안도현</div>
+          <div className={style.subtitle}>계속 내려서 더-보기</div>
+        </div>
+        <div className={style.background} style={{ opacity: backgroundOpacity }}>
+          <div className={style.background_text}>
+            <div className={style.title}>저의 작은 경험이 귀사에 도움이 되길 바랍니다.</div>
+            <div className={style.subtitle}>사용자에게 좋은 서비스를 만들기 위해 노력하고있습니다.</div>
+          </div>
+        </div>
       </div>
+      <div ref={refBackgroundPlaceholder} style={{ height: backgroundHeight }}></div>
     </div>
   )
 }
