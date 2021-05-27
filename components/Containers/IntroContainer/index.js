d9de6d8876b335d3cb9c4aab3a17ded06abb8391
devDohyun <dohyun10101@gmail.com>
2021-05-27 20:49:57 +0900

    deleted unused

--- components/Containers/IntroContainer/index.js
+++ components/Containers/IntroContainer/index.js
@@ -1,21 +1,9 @@
 import Intro from '@/components/Intro'
-import CodeItem from '@/components/CodeItem'
 
 
 const IntroContainer = () => {
-  const codeItemTexts = [
-    `<div classNames={style.nextJS}>\n\t{text}\n</div>`,
-    `<template #content="{ text }">\n\t{{ text }}의 Nuxt JS App\n</template>`,
-    `const express = require('express')\nconst router = express.Router()`
-  ]
-  
-  
   return (
-    <Intro
-      codeItems={
-        codeItemTexts.map((text) => <CodeItem key={text} text={text} />)
-      }
-    ></Intro>
+    <Intro></Intro>
   )
 }
 
--- components/Intro/index.js
+++ components/Intro/index.js
@@ -10,9 +10,6 @@ const Intro = (props) => {
         <div className={style.title}>Front-End 개발자 안도현</div>
         <div className={style.subtitle}>아래로 내려서 더-보기</div>
       </div>
-      <div className={style.background}>
-        {props.codeItems}
-      </div>
     </div>
   )
 }
