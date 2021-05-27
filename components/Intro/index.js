b21ac156a83fce60bc13d60bf5c88732d7c9565d
devDohyun <dohyun10101@gmail.com>
2021-05-27 19:45:34 +0900

    Modified text

--- components/Intro/index.js
+++ components/Intro/index.js
@@ -8,8 +8,8 @@ const Intro = (props) => {
   return (
     <div className={style.intro}>
       <div>
-        <div className={style.title}>Javascript Fullstack 개발자 안도현</div>
-        <div className={style.subtitle}>아래로 내려서 더 보기</div>
+        <div className={style.title}>Front-End 개발자 안도현</div>
+        <div className={style.subtitle}>아래로 내려서 더-보기</div>
       </div>
       <div className={style.background}>
         {props.codeItems}
