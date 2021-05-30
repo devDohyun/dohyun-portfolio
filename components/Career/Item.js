57a260b30e7258ba056f96674b3dca872bc5b448
devDohyun <dohyun10101@gmail.com>
2021-05-30 13:40:00 +0900

    updated

--- components/Career/Item.js
+++ components/Career/Item.js
@@ -1,14 +1,28 @@
-import { useMemo } from 'react'
+import { useCallback, useMemo } from 'react'
 import Icon from '@mdi/react'
 import { mdiPlusCircleOutline } from '@mdi/js'
 import style from './Item.module.scss'
 
 
-const CareerItem = ({ name, description, start, end, position }) => {
+const CareerItem = ({ id, name, description, start, end, position, onButtonClick }) => {
   const period = useMemo(() => {
-    return start
-  }, [start, end])
+    let result = start
+    result += ' ~ '
 
+    if (end) result += end
+    else result += '현재'
+    
+    return result
+  }, [start, end])
+  const handleMoreButtonClick = useCallback((e) => {
+    if (typeof onButtonClick !== 'function') return
+
+
+    onButtonClick({
+      event: e,
+      id
+    })
+  }, [id, onButtonClick])
 
   return (
     <div className={style.career_item}>
@@ -16,7 +30,7 @@ const CareerItem = ({ name, description, start, end, position }) => {
         <div className={style.name}>{name}</div>
         <div className={style.period}>{period}</div>
       </div>
-      <button className={style.button_more} type="button">
+      <button className={style.button_more} type="button" onClick={handleMoreButtonClick}>
         <Icon size="1em" path={mdiPlusCircleOutline} />
       </button>
     </div>
