61e788a507fd57086a19c6b4a1deca7d954c96a9
devDohyun <dohyun10101@gmail.com>
2021-06-03 19:58:33 +0900

    updated

--- components/Stacks/index.js
+++ components/Stacks/index.js
@@ -1,4 +1,6 @@
 import ImageCard from '@/components/Card/Image'
+import Card from '../Card'
+import CardTitle from '../Card/Title/Title'
 import style from './index.module.scss'
 import StackItem from './Item'
 import StackMainItem from './Item/Main'
@@ -11,46 +13,63 @@ const Stacks = ({ stacks = [] }) => {
           <div>
             <ImageCard src="https://storage.googleapis.com/clone-youtube-music/devdohyun-portfolio/imdohyun.jpg" ratio={1.2} />
           </div>
-          <div className={style.title_card}>
-            <div className={style.text_javascript}>JS</div>
-            <div className={style.title}>
-              Javascript를 활용하여 <span className={style.highlight}>간결하고 정확한 코딩</span>을 지향합니다.
-            </div>
+          <div>
+            <Card>
+              <div className={style.title_card}>
+                <div className={style.text_javascript}>JS</div>
+                <CardTitle>
+                  <div className={style.title}>
+                    Javascript를 활용하여 <span className={style.highlight}>간결하고 정확한 코딩</span>을 지향합니다.
+                  </div>
+                </CardTitle>
+              </div>
+            </Card>
           </div>
-          <div className={style.stack_card}>
-            <div className={style.title}>
-              제가 주로 다루고있는 <span className={style.highlight}>기술스택</span>은 ...
-            </div>
-            <div className={style.main_wrapper}>
-              {stacks
-                .filter(item => item.isMain === true)
-                .map(item => (
-                  <StackMainItem key={item.name} name={item.name} level={item.level} isLearning={item.isLearning} />
-                ))}
-            </div>
-            <div className={style.others_wrapper}>
-              {stacks
-                .filter(item => item.isMain !== true)
-                .map(item => (
-                  <StackItem key={item.name} name={item.name} />
-                ))}
-            </div>
+          <div>
+            <Card>
+              <div className={style.stack_card}>
+                <CardTitle>
+                  <div className={style.title}>
+                    제가 주로 다루고있는 <span className={style.highlight}>기술스택</span>은 ...
+                  </div>
+                </CardTitle>
+                <div className={style.main_wrapper}>
+                  {stacks
+                    .filter(item => item.isMain === true)
+                    .map(item => (
+                      <StackMainItem key={item.name} name={item.name} level={item.level} isLearning={item.isLearning} />
+                    ))}
+                </div>
+                <div className={style.others_wrapper}>
+                  {stacks
+                    .filter(item => item.isMain !== true)
+                    .map(item => (
+                      <StackItem key={item.name} name={item.name} />
+                    ))}
+                </div>
+              </div>
+            </Card>
           </div>
-          <div className={style.achievement_card}>
-            <div className={style.title}>
-              <span className={style.highlight}>소프트웨어 특성화 고등학교</span>를 <span className={style.highlight}>졸업</span>하였습니다.
-            </div>
-            <div className={style.item_wrapper}>
-              어린 시절부터 컴퓨터와 관련된 직업을 가지고 싶었던 저는, 소프트웨어 특성화 고등학교인 <span className={style.highlight}>세명 컴퓨터고등학교</span>에 진학하여{' '}
-              <span className={style.highlight}>스마트 보안솔루션과</span>를 전공하였습니다.
-              <sup className={style.note} title="2018년 2월 졸업">
-                1
-              </sup>
-              <br />
-            </div>
-            <div className={style.note_wrapper}>
-              <div className={style.item}>1. 2018년 2월 졸업</div>
-            </div>
+          <div>
+            <Card>
+              <div className={style.achievement_card}>
+                <CardTitle>
+                  <div className={style.title}>
+                    <span className={style.highlight}>소프트웨어 특성화 고등학교</span>를 <span className={style.highlight}>졸업</span>하였습니다.
+                  </div>
+                </CardTitle>
+                <div className={style.item_wrapper}>
+                  어린 시절부터 컴퓨터와 관련된 직업을 가지고 싶었던 저는, 소프트웨어 특성화 고등학교인 <span className={style.highlight}>세명 컴퓨터고등학교</span>에 진학하여 <span className={style.highlight}>스마트 보안솔루션과</span>를 전공하였습니다.
+                  <sup className={style.note} title="2018년 2월 졸업">
+                    1
+                  </sup>
+                  <br />
+                </div>
+                <div className={style.note_wrapper}>
+                  <div className={style.item}>1. 2018년 2월 졸업</div>
+                </div>
+              </div>
+            </Card>
           </div>
         </div>
       </div>
