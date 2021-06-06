e55400e1a0daffc0a0126640ef99d6beaec201ac
devDohyun <dohyun10101@gmail.com>
2021-06-07 07:07:32 +0900

    updated

--- components/Project/index.js
+++ components/Project/index.js
@@ -5,7 +5,13 @@ const Project = ({ projects = [], onClickActiveButton }) => {
   return (
     <div className={style.project}>
       <div className="container">
+        <div className={style.title}>
+          지금까지 맡아온
+          <br />
+          크고작은 프로젝트.
+        </div>
         <ProjectList items={projects} onClickActiveButton={onClickActiveButton} />
+        <div className={style.more_text}>외 7개의 더 작은 프로젝트에도 참여하였습니다.</div>
       </div>
     </div>
   )
