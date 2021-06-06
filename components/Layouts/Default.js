5bb55c1376f07571ccdebeb695be1429d30f16fc
devDohyun <dohyun10101@gmail.com>
2021-06-07 07:18:47 +0900

    Added head

--- components/Layouts/Default.js
+++ components/Layouts/Default.js
@@ -1,10 +1,17 @@
+import Head from 'next/head'
+
 const Default = (props) => {
   return (
     <>
+      <Head>
+        <title>개발자 안도현 포트폴리오</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="개발자 안도현의 포트폴리오입니다." />
+        <meta property="og:image" content="https://storage.googleapis.com/clone-youtube-music/devdohyun-portfolio/my-memoji.png" />
+      </Head>
       {props.children}
     </>
   )
 }
-
 
 export default Default
