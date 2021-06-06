c4050c27bc6c6036ebcbba9e8bc42582c36c8b3d
devDohyun <dohyun10101@gmail.com>
2021-06-07 00:26:20 +0900

    Added feature: lock background when modal activated

--- components/Modal/index.js
+++ components/Modal/index.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import cx from 'classnames'
 import style from './index.module.scss'
 
+const lockBackground = () => {
+  const body = document.body
+  const store = {
+    position: body.style.position,
+  }
+  const scrollY = window.scrollY
+
+  body.style.position = 'fixed'
+  body.style.top = `-${scrollY}px`
+  body.style.width = '100%'
+  body.style.height = '100%'
+
+  return store
+}
+const releaseBackground = (store = {}) => {
+  const body = document.body
+  const top = body.style.top
+
+  body.style.position = store.position || ''
+  body.style.top = ''
+  body.style.width = ''
+  body.style.height = ''
+
+  if (top) window.scrollTo({ top: parseInt(top) * -1 })
+}
+
 const Modal = ({ children, active, onBackgroundClick }) => {
+  const [bodyState, setBodyState] = useState({ isLocked: false, styles: {} })
+
+  useEffect(() => {
+    if (active) setBodyState((state) => ({ ...state, isLocked: true, styles: lockBackground() }))
+    else if (active === false) {
+      if (bodyState.isLocked) releaseBackground(bodyState.styles)
+      setBodyState((state) => ({ ...state, isLocked: false, styles: {} }))
+    }
+  }, [active])
+  useEffect(() => releaseBackground, [])
+
   return (
     <div className={cx(style.modal, { [style.active]: active })}>
       <div onClick={onBackgroundClick} className={style.dimmed}></div>
